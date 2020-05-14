e5dfc865746a1014a78901f6943115edc20844d8
Richard Moore <github@ricmoo.com>
2020-05-14 00:37:44 -0400

    Updated dist files.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,7 @@
-export { Node, TextNode, ElementNode, LinkNode, ListNode, PropertyNode, ElementStyle, MarkdownStyle, escapeText, parseBlock, parseMarkdown } from "./markdown";
-export { Fragment, CodeFragment, TocFragment, Page, Document, TocEntry, FragmentType } from "./document";
+export { Node, TextNode, ElementNode, LinkNode, ListNode, PropertyNode, CellNode, SymbolNode, SymbolNames, CellAlign, ElementStyle, MarkdownStyle, escapeText, expandMacro, parseBlock, parseMarkdown, StylesAll, StylesInline } from "./markdown";
+export { Fragment, CodeFragment, TableFragment, TocFragment, Page, Document, TableStyle, TocEntry, FragmentType } from "./document";
 export { Config, ConfigLink, MarkdownConfig } from "./config";
 export { Line, Script } from "./script";
 export { File, Renderer } from "./renderer";
-export { HtmlRenderer } from "./renderer-html";
+export { HtmlRenderer, SinglePageHtmlRenderer } from "./renderer-html";
 export { MarkdownRenderer } from "./renderer-markdown";
