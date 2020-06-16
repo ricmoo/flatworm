3e92b9e04f6940ba3d5a2504f101c84ddd010c20
Richard Moore <github@ricmoo.com>
2020-06-15 20:23:36 -0400

    Added metadata renderer.

---
+++ src.ts/renderer-metadata.ts
@@ -0,0 +1,25 @@
+import { Document } from "./document";
+
+import { File, Renderer } from "./renderer";
+
+export class MetadataRenderer extends Renderer {
+    constructor(filename?: string) {
+        super(filename || "metadata.json");
+    }
+
+    renderDocument(document: Document): Array<File> {
+        const links: { [ name: string ]: string } = { };
+        document.names.forEach((name) => {
+            links[name] = document.getLinkUrl(name);
+        });
+        const metadata = {
+            version: "0.1",
+            links: links
+        };
+
+        return [ {
+            filename: document.config.getPath("/" + this.filename).substring(1),
+            content: JSON.stringify(metadata)
+        } ];
+    }
+}
