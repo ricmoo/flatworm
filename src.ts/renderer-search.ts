11db8003f4236a9eaae1fc29f992a2bf28c93a03
Richard Moore <github@ricmoo.com>
2020-10-21 04:01:20 -0400

    Create more reliable links to content in search results.

--- src.ts/renderer-search.ts
+++ src.ts/renderer-search.ts
@@ -43,6 +43,7 @@ export class SearchRenderer extends Renderer {
                     pretitle.push(section);
                 }
 
+                const titleWords: Array<string> = [ ];
                 if (fragment.title) {
                     let title = fragment.title.textContent.trim();
                     if (title) {
@@ -52,30 +53,73 @@ export class SearchRenderer extends Renderer {
                         }
                         pretitle.push(title);
                         summary.title = pretitle.join(" -- ");
+
+                        title.replace(/([a-z][a-z0-9]+)/ig, (all, word) => {
+
+                            // Get each component of a camel case word
+                            const camelCases = word.split(/(^[a-z]|[A-Z])/);
+                            if (camelCases.length > 3) {
+                                for (let i = 1; i < camelCases.length; i += 2) {
+                                    const word = (camelCases[i] + camelCases[i + 1]).toLowerCase();
+                                    if (titleWords.indexOf(word) === -1) { titleWords.push(word); }
+                                }
+                            }
+
+                            word = word.toLowerCase();
+                            if (titleWords.indexOf(word) === -1) { titleWords.push(word); }
+
+                            return "";
+                        });
                     }
                 }
                 if (fragment.link) {
+                    // Link directly to this fragment
                     link = (page.path + "#" + fragment.link);
+                } else {
+                    const parent = fragment.parent;
+                    if (parent && parent.link) {
+                        // Link to our section/subsection/heading
+                        link = (page.path + "#" + parent.link);
+                    } else {
+                        // Link to our page (better than nothing)
+                        link = page.path;
+                    }
                 }
-                fragment.body.forEach((node) => {
+
+                let baseTag:string = null;
+
+                fragment.body.forEach((node, index) => {
                     node.textContent.split(/\.( |$)/).forEach((sentence) => {
                         const text = sentence.trim() + ".";
                         if (text === ".") { return; }
                         summary.blocks.push({ link, text });
 
-                        const dedup: { [ word: string ]: boolean } = { };
                         sentence.replace(/([a-z][a-z0-9]+)/ig, (all, word) => {
                             word = "_" + word.toLowerCase();
+
+                            // The tag to the summary block
+                            const tag = `${ summaries.length }/${ summary.blocks.length - 1 }`
+                            if (baseTag == null) { baseTag = tag; }
+
+                            // Link the word to the tag
                             if (indices[word] == null) { indices[word] = [ ]; }
-                            if (!dedup[word]) {
-                                indices[word].push(`${ summaries.length }/${ summary.blocks.length - 1 }`);
-                                dedup[word] = true;
+                            if (indices[word].indexOf(tag) === -1) {
+                                indices[word].push(tag);
                             }
 
                             return "";
                         });
                     });
                 });
+
+                // Link the first paragraph to each word in the title
+                if (baseTag) {
+                    titleWords.forEach((word) => {
+                        word = "_" + word;
+                        if (indices[word] == null) { indices[word] = [ ]; }
+                        if (indices[word].indexOf(baseTag) === -1) { indices[word].push(baseTag); }
+                    });
+                }
             });
 
             if (summary.title != null && summary.blocks.length) {
